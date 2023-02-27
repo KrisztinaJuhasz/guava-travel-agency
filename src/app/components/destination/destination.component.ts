af9251aa3cb0e4f153c7fbca2755668133b35736
MyoMax39 <kaman.krisztina.m@gmail.com>
2023-02-27 10:14:19 +0100

    typography and text fix

--- src/app/components/destination/destination.component.ts
+++ src/app/components/destination/destination.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-destination',
@@ -9,13 +11,22 @@ export class DestinationComponent implements OnInit, AfterViewInit {
 
   @Input('countryName')
   countryName: string;
+
   @Input('countryDescription')
   countryDescription: string;
+
   @Input('countryImage')
   countryImage: string;
+
   @ViewChild('destinationImage')
   destinationImageRef: ElementRef<HTMLDivElement>;
   destinationImage: HTMLDivElement
+
+  @ViewChild('destinationText')
+  destionationTextRef: ElementRef<HTMLParagraphElement>;
+  destinationText: HTMLParagraphElement;
+
+  textFade: boolean;
 
   constructor() {
     this.countryName = '';
@@ -23,6 +34,9 @@ export class DestinationComponent implements OnInit, AfterViewInit {
     this.countryImage = '';
     this.destinationImageRef = {} as ElementRef<HTMLDivElement>;
     this.destinationImage = {} as HTMLDivElement;
+    this.destionationTextRef = {} as ElementRef<HTMLParagraphElement>;
+    this.destinationText = {} as HTMLParagraphElement;
+    this.textFade = true;
   }
 
   ngOnInit(): void {
@@ -31,6 +45,19 @@ export class DestinationComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     this.destinationImage = this.destinationImageRef.nativeElement;
     this.destinationImage.style.backgroundImage = `url(${this.countryImage})`;
+    this.destinationText = this.destionationTextRef.nativeElement;
+
+    const scroll$: Observable<WheelEvent> = fromEvent<WheelEvent>(this.destinationText, 'wheel');
+
+    scroll$
+      .pipe(
+        map(() => this.destinationText.scrollHeight - this.destinationText.scrollTop <= this.destinationText.clientHeight),
+        debounceTime(200),
+        distinctUntilChanged()
+      )
+      .subscribe((isAtBottom: boolean) => {
+        this.textFade = !isAtBottom;
+      });
   }
 
 }
